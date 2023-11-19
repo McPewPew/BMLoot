7d742e202043455c7cc45c383d6a7739715927f9
McPewPew <98543024+McPewPew@users.noreply.github.com>
2023-11-19 03:54:41 +0000

    Update BMLoot.lua

--- BMLoot.lua
+++ BMLoot.lua
@@ -1,15 +1,20 @@
-DEFAULT_CHAT_FRAME:AddMessage("Testing, testing", 1, 0, 1);
+DEFAULT_CHAT_FRAME:AddMessage("BMLoot has not let you down yet...", 1, 0, 1);
 BMLoot = CreateFrame("Frame")
 BMLoot:RegisterEvent("START_LOOT_ROLL") 
 BMLoot:RegisterEvent("LOOT_BIND_CONFIRM")
 BMLoot:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 BMLoot:SetScript("OnEvent", function()
-    if event == "START_LOOT_ROLL" then
-        local rollID, rollTime, rollItemLink, rollQuantity, rollPlayer = arg1, arg2, arg3, arg4, arg5
-		local texture, name, count, quality = GetLootRollItemInfo(rollID)
+if event == "START_LOOT_ROLL" then
+	local rollID, rollTime, rollItemLink, rollQuantity, rollPlayer = arg1, arg2, arg3, arg4, arg5
+	local texture, name, count, quality = GetLootRollItemInfo(rollID)
         if GetZoneText() == "The Black Morass" and (name == "Corrupted Sand" or name == "Arcane Essence") then
-            RollOnLoot(rollID, 1)
-			StaticPopup1Button1:Click()		
+        	RollOnLoot(rollID, 1)
+		for i=1,STATICPOPUP_NUMDIALOGS do -- for i=1 to (I think this is always 4 - STATICPOPUP_NUMDIALOGS)
+			local frame = getglobal("StaticPopup"..i)  -- set frame to be = StaticPopup1, StaticPopup2 etc
+			if frame:IsShown() and frame.which = "CONFIRM_LOOT_ROLL" then -- what is the StaticPopup for 
+				getglobal("StaticPopup"..i.."Button1"):Click(); --click StaticPopup..i..Button1:Click()
+			end
+		end
         end	
 	elseif event == "ZONE_CHANGED_NEW_AREA" then
 		if GetRealZoneText() == "The Black Morass" then
@@ -17,3 +22,6 @@ BMLoot:SetScript("OnEvent", function()
 		end
 	end
 end)
+
+--If not UnitIsDeadOrGhost("player") then 
+--something to test later
